bc7c5aeab2977c911fff335ee07a4545e8a8731d
Fernando Lopez <fer68769@gmail.com>
2022-05-27 12:39:07 -0500

    Confirmar cuenta de usuario

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const User = require('../models/user');
+const { nanoid } = require('nanoid');
 
 const registerForm = (req, res) => {
     res.render('register');
@@ -12,9 +12,29 @@ const registerPost = async(req, res) => {
         const user = await User.findOne({ email });
         if (user) throw new Error('Ya existe el usuario');
 
-        let newUser = new User({ name, email, password });
+        let newUser = new User({ name, email, password, tokenConfirm: nanoid(6) });
         await newUser.save();
-        res.json(newUser);
+
+        // Enviar correo electrónico con confirmación cuenta
+
+        res.redirect('/auth/login');
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ error: error.message });
+    }
+}
+
+const confirmarCuenta = async(req, res) => {
+    const { token } = req.params;
+    try {
+        const user = await User.findOne({ tokenConfirm: token });
+        if (!user) throw new Error('No existe el usuario');
+
+        user.countConfirm = true;
+        user.tokenConfirm = null;
+
+        await user.save();
+        res.redirect('/auth/login');
     } catch (error) {
         console.log(error);
         res.status(404).json({ error: error.message });
@@ -29,5 +49,6 @@ const loginForm = async(req, res) => {
 module.exports = {
     loginForm,
     registerForm,
-    registerPost
+    registerPost,
+    confirmarCuenta
 }
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { loginForm, registerForm, registerPost } = require('../controllers/authController');
+const { loginForm, registerForm, registerPost, confirmarCuenta } = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/login', loginForm);
 router.get('/register', registerForm);
+router.get('/confirmarCuenta/:token', confirmarCuenta);
 router.post('/register', registerPost);
 
 module.exports = router;
