f24d56f2484984175d4b3b6af40f1fc37534e64a
Fernando Lopez <fer68769@gmail.com>
2022-06-09 12:56:05 -0500

    Se instalo y configuro csrf token

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const passport = require('passport');
 const port = process.env.PORT || 5000;
 const path = require('path');
 const { create } = require('express-handlebars');
-
+const csrf = require('csurf');
 
 const app = express();
 
@@ -43,9 +43,17 @@ app.engine(".hbs", hbs.engine);
 app.set('view engine', 'hbs');
 app.set("views", "./views");
 
-
 // Midleware
 app.use(express.urlencoded({ extended: true }));
+
+// CSRF TOKEN
+app.use(csrf());
+app.use((req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', req.csrfToken())
+    next();
+})
+
 app.use('/', require('./routes/home'));
 app.use('/auth', require('./routes/login'));
 app.use(express.static(__dirname + '/public'));
